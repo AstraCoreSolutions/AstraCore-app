307a3b32fb95158702e6af544dcb0a82c771377c
AstraCoreSolutions <info@astracore-solutions.cz>
2025-06-06 19:10:13 +0200

    Create index.js

---
+++ src/store/index.js
@@ -0,0 +1,4 @@
+export { default as useAuthStore } from './authStore'
+export { default as useProjectsStore } from './projectsStore'
+export { default as useFinanceStore } from './financeStore'
+export { default as useEmployeesStore } from './employeesStore'
