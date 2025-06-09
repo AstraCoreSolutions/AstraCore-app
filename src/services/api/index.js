93bf065b83d13745a176c18b4acadbb26a828550
AstraCoreSolutions <info@astracore-solutions.cz>
2025-06-09 12:59:18 +0200

    Update index.js

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,5 +1,20 @@
 export { default as authAPI } from './auth.js'
 export { default as projectsAPI } from './projects.js'
+export { default as clientsAPI } from './clients.js'
+export { default as materialsAPI } from './materials.js'
+export { default as equipmentAPI } from './equipment.js'
+export { default as vehiclesAPI } from './vehicles.js'
+export { default as suppliersAPI } from './suppliers.js'
+export { default as financeAPI } from './finance.js'
 
 // Re-export for convenience
-export { authAPI, projectsAPI }
+export { 
+  authAPI, 
+  projectsAPI, 
+  clientsAPI, 
+  materialsAPI, 
+  equipmentAPI, 
+  vehiclesAPI, 
+  suppliersAPI, 
+  financeAPI 
+}
